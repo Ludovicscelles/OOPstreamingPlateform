d7e935462c8c874d22838ca43b949968ab53cc2f
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-30 11:37:50 +0200

    feat(tvshow) : add TvShowData file

---
+++ src/data/TvShowData.ts
@@ -0,0 +1,238 @@
+import { time } from "console";
+
+export const tvShowData = [
+  {
+    id: "T001",
+    title: "Les Traitres",
+    genre: "game-show",
+    director: "Françis Côté",
+    seasonData: [
+      {
+        seasonYear: 2022,
+        seasonNumber: 1,
+        TVHost: "Éric Antoine",
+        episodes: [
+          {
+            title: "Épisode 1",
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            title: "Épisode 2",
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            title: "Épisode 3",
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            title: "Épisode 4",
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            title: "Épisode 5",
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            title: "Épisode 6",
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            title: "Épisode 7",
+            numberEpisode: 7,
+            duration: 60,
+          },
+        ],
+      },
+      {
+        seasonYear: 2023,
+        seasonNumber: 2,
+        TVHost: "Éric Antoine",
+        episodes: [
+          {
+            title: "Épisode 1",
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            title: "Épisode 2",
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            title: "Épisode 3",
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            title: "Épisode 4",
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            title: "Épisode 5",
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            title: "Épisode 6",
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            title: "Épisode 7",
+            numberEpisode: 7,
+            duration: 60,
+          },
+          {
+            title: "Épisode 8",
+            numberEpisode: 8,
+            duration: 60,
+          },
+          {
+            title: "Épisode 9",
+            numberEpisode: 9,
+            duration: 60,
+          },
+          {
+            title: "Épisode 10",
+            numberEpisode: 10,
+            duration: 60,
+          },
+        ],
+      },
+      {
+        seasonYear: 2024,
+        seasonNumber: 3,
+        TVHost: "Éric Antoine",
+        episodes: [
+          {
+            title: "Épisode 1",
+            numberEpisode: 1,
+            duration: 60,
+          },
+          {
+            title: "Épisode 2",
+            numberEpisode: 2,
+            duration: 60,
+          },
+          {
+            title: "Épisode 3",
+            numberEpisode: 3,
+            duration: 60,
+          },
+          {
+            title: "Épisode 4",
+            numberEpisode: 4,
+            duration: 60,
+          },
+          {
+            title: "Épisode 5",
+            numberEpisode: 5,
+            duration: 60,
+          },
+          {
+            title: "Épisode 6",
+            numberEpisode: 6,
+            duration: 60,
+          },
+          {
+            title: "Épisode 7",
+            numberEpisode: 7,
+            duration: 60,
+          },
+          {
+            title: "Épisode 8",
+            numberEpisode: 8,
+            duration: 60,
+          },
+          {
+            title: "Épisode 9",
+            numberEpisode: 9,
+            duration: 60,
+          },
+          {
+            title: "Épisode 10",
+            numberEpisode: 10,
+            duration: 60,
+          },
+          {
+            time: "Épisode 11",
+            numberEpisode: 11,
+            duration: 60,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "T002",
+    title: "Fort Boyard",
+    genre: "game-show",
+    director: "Françis Côté",
+    seasonData: [
+      {
+        seasonYear: 2022,
+        seasonNumber: 34,
+        TVHost: "Olivier Minne",
+        episodes: [
+          {
+            title: "Épisode 1",
+            numberEpisode: 1,
+            duration: 90,
+          },
+          {
+            title: "Épisode 2",
+            numberEpisode: 2,
+            duration: 90,
+          },
+          {
+            title: "Épisode 3",
+            numberEpisode: 3,
+            duration: 90,
+          },
+          {
+            title: "Épisode 4",
+            numberEpisode: 4,
+            duration: 90,
+          },
+          {
+            title: "Épisode 5",
+            numberEpisode: 5,
+            duration: 90,
+          },
+          {
+            title: "Épisode 6",
+            numberEpisode: 6,
+            duration: 90,
+          },
+          {
+            title: "Épisode 7",
+            numberEpisode: 7,
+            duration: 90,
+          },
+          {
+            title: "Épisode 8",
+            numberEpisode: 8,
+            duration: 90,
+          },
+          {
+            title: "Épisode 9",
+            numberEpisode: 9,
+            duration: 90,
+          },
+          {
+            title: "Épisode 10",
+            numberEpisode: 10,
+            duration: 90,
+          },
+        ],
+      },
+    ],
+  },
+];
