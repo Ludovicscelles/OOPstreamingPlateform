5640c07b06dfe096b58bd7364ba3ca59b5d1ee16
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-07-07 17:14:54 +0200

    feat(tvshow): fix data by removing episode's title and rename 'Les traîtres' with circumflex

--- src/data/TvShowData.ts
+++ src/data/TvShowData.ts
@@ -1,49 +1,40 @@
-import { time } from "console";
-
 export const tvShowData = [
   {
     id: "T001",
-    title: "Les Traitres",
+    title: "Les Traîtres",
     genre: "game-show",
     director: "Françis Côté",
     seasonData: [
       {
         seasonYear: 2022,
         seasonNumber: 1,
-        TVHost: "Éric Antoine",
+        seasonTVHost: "Éric Antoine",
         episodes: [
           {
-            title: "Épisode 1",
             numberEpisode: 1,
             duration: 60,
           },
           {
-            title: "Épisode 2",
             numberEpisode: 2,
             duration: 60,
           },
           {
-            title: "Épisode 3",
             numberEpisode: 3,
             duration: 60,
           },
           {
-            title: "Épisode 4",
             numberEpisode: 4,
             duration: 60,
           },
           {
-            title: "Épisode 5",
             numberEpisode: 5,
             duration: 60,
           },
           {
-            title: "Épisode 6",
             numberEpisode: 6,
             duration: 60,
           },
           {
-            title: "Épisode 7",
             numberEpisode: 7,
             duration: 60,
           },
@@ -52,55 +43,45 @@ export const tvShowData = [
       {
         seasonYear: 2023,
         seasonNumber: 2,
-        TVHost: "Éric Antoine",
+        seasonTVHost: "Éric Antoine",
         episodes: [
           {
-            title: "Épisode 1",
             numberEpisode: 1,
             duration: 60,
           },
           {
-            title: "Épisode 2",
             numberEpisode: 2,
             duration: 60,
           },
           {
-            title: "Épisode 3",
             numberEpisode: 3,
             duration: 60,
           },
           {
-            title: "Épisode 4",
             numberEpisode: 4,
             duration: 60,
           },
           {
-            title: "Épisode 5",
             numberEpisode: 5,
             duration: 60,
           },
           {
-            title: "Épisode 6",
             numberEpisode: 6,
             duration: 60,
           },
           {
-            title: "Épisode 7",
             numberEpisode: 7,
             duration: 60,
           },
           {
-            title: "Épisode 8",
             numberEpisode: 8,
             duration: 60,
           },
           {
-            title: "Épisode 9",
             numberEpisode: 9,
             duration: 60,
           },
           {
-            title: "Épisode 10",
             numberEpisode: 10,
             duration: 60,
           },
@@ -109,60 +90,49 @@ export const tvShowData = [
       {
         seasonYear: 2024,
         seasonNumber: 3,
-        TVHost: "Éric Antoine",
+        seasonTVHost: "Éric Antoine",
         episodes: [
           {
-            title: "Épisode 1",
             numberEpisode: 1,
             duration: 60,
           },
           {
-            title: "Épisode 2",
             numberEpisode: 2,
             duration: 60,
           },
           {
-            title: "Épisode 3",
             numberEpisode: 3,
             duration: 60,
           },
           {
-            title: "Épisode 4",
             numberEpisode: 4,
             duration: 60,
           },
           {
-            title: "Épisode 5",
             numberEpisode: 5,
             duration: 60,
           },
           {
-            title: "Épisode 6",
             numberEpisode: 6,
             duration: 60,
           },
           {
-            title: "Épisode 7",
             numberEpisode: 7,
             duration: 60,
           },
           {
-            title: "Épisode 8",
             numberEpisode: 8,
             duration: 60,
           },
           {
-            title: "Épisode 9",
             numberEpisode: 9,
             duration: 60,
           },
           {
-            title: "Épisode 10",
             numberEpisode: 10,
             duration: 60,
           },
           {
-            time: "Épisode 11",
             numberEpisode: 11,
             duration: 60,
           },
@@ -179,55 +149,45 @@ export const tvShowData = [
       {
         seasonYear: 2022,
         seasonNumber: 34,
-        TVHost: "Olivier Minne",
+        seasonTVHost: "Olivier Minne",
         episodes: [
           {
-            title: "Épisode 1",
             numberEpisode: 1,
             duration: 90,
           },
           {
-            title: "Épisode 2",
             numberEpisode: 2,
             duration: 90,
           },
           {
-            title: "Épisode 3",
             numberEpisode: 3,
             duration: 90,
           },
           {
-            title: "Épisode 4",
             numberEpisode: 4,
             duration: 90,
           },
           {
-            title: "Épisode 5",
             numberEpisode: 5,
             duration: 90,
           },
           {
-            title: "Épisode 6",
             numberEpisode: 6,
             duration: 90,
           },
           {
-            title: "Épisode 7",
             numberEpisode: 7,
             duration: 90,
           },
           {
-            title: "Épisode 8",
             numberEpisode: 8,
             duration: 90,
           },
           {
-            title: "Épisode 9",
             numberEpisode: 9,
             duration: 90,
           },
           {
-            title: "Épisode 10",
             numberEpisode: 10,
             duration: 90,
           },
